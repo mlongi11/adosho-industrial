3c37b7ba1b1afacebd340dea550f610348a88321
mlongi11 <mlongi@chicagobooth.edu>
2022-05-24 21:20:13 +0000

    updated relations and server working now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,25 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
 
   has_many :preferences, class_name: "BreedPreference", foreign_key: "user_id", dependent: :destroy
-
+  has_one :interest, foreign_key: "user_id", dependent: :destroy
+  has_one :watchlist, class_name: "Watchlist", foreign_key: "user_id", dependent: :destroy
+  has_one :match_list, class_name: "Match", foreign_key: "user_id", dependent: :destroy
         
   # has_many(:preferences, { :class_name => "BreedPreference", :foreign_key => "user_id", :dependent => :destroy })
   # has_one(:interest, { :class_name => "Interest", :foreign_key => "user_id", :dependent => :destroy })
-  # has_many(:pets, { :class_name => "Pet", :foreign_key => "adopter_id", :dependent => :destroy })
   # has_many(:watchlists, { :class_name => "Watchlist", :foreign_key => "user_id", :dependent => :destroy })
   # has_many(:matches, { :class_name => "Match", :foreign_key => "user_id", :dependent => :destroy })
+
+  has_many :watched_pets, through: :watchlists, source: :pet
+  has_many :matched_pets, through: :matches, source: :pet
 
   # has_many(:watched_pets, { :through => :watchlists, :source => :pet })
   # has_many(:matched_pets, { :through => :matches, :source => :pet })
 
-  # validates(:phone_number, { :format => { :with => /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/ } })
-  # validates(:phone_number, { :presence => true })
-  # validates(:account_type, { :inclusion => { :in => [ "User", "Administrator" ] } })
+  validates(:phone_number, { :format => { :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/ } })
+  validates(:phone_number, { :presence => true })
+  validates(:account_type, { :inclusion => { :in => [ "User", "Administrator" ] } })
 
-  # enum account_type: [ :User, :ADministrator ]
+  enum account_type: [ :User, :Administrator ]
 
 end
