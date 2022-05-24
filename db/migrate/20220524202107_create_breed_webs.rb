48dc3d81b5ae638b4ec4dd32cf41f3599f281ca3
mlongi11 <mlongi@chicagobooth.edu>
2022-05-24 20:32:08 +0000

    db migrate ran

--- db/migrate/20220524202107_create_breed_webs.rb
+++ db/migrate/20220524202107_create_breed_webs.rb
@@ -1,8 +1,8 @@
 class CreateBreedWebs < ActiveRecord::Migration[6.1]
   def change
     create_table :breed_webs do |t|
-      t.references :primary_breed, null: false, foreign_key: true
-      t.references :similar_breed, null: false, foreign_key: true
+      t.references :primary_breed, null: false, foreign_key: {to_table: :breeds}
+      t.references :similar_breed, null: false, foreign_key: {to_table: :breeds}
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_201504) do
+ActiveRecord::Schema.define(version: 2022_05_24_202154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "breed_preferences", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "breed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["breed_id"], name: "index_breed_preferences_on_breed_id"
+    t.index ["user_id"], name: "index_breed_preferences_on_user_id"
+  end
+
+  create_table "breed_webs", force: :cascade do |t|
+    t.bigint "primary_breed_id", null: false
+    t.bigint "similar_breed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["primary_breed_id"], name: "index_breed_webs_on_primary_breed_id"
+    t.index ["similar_breed_id"], name: "index_breed_webs_on_similar_breed_id"
+  end
+
+  create_table "breeds", force: :cascade do |t|
+    t.string "species"
+    t.string "breed_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "interests", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.boolean "interested_in_fostering_dogs"
+    t.boolean "interested_in_fostering_cats"
+    t.boolean "interested_in_adopting_dogs"
+    t.boolean "interested_in_adopting_cats"
+    t.integer "children_competability_preference"
+    t.integer "dog_competability_preference"
+    t.integer "cat_competability_preference"
+    t.integer "activity_competability_preference"
+    t.integer "home_alone_competability_preference"
+    t.integer "human_sociability_competability_preference"
+    t.string "enrichment_competability_preference"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_interests_on_user_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.bigint "pet_id", null: false
+    t.bigint "user_id", null: false
+    t.float "match_strength"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_id"], name: "index_matches_on_pet_id"
+    t.index ["user_id"], name: "index_matches_on_user_id"
+  end
+
+  create_table "pet_breeds", force: :cascade do |t|
+    t.bigint "breed_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["breed_id"], name: "index_pet_breeds_on_breed_id"
+    t.index ["pet_id"], name: "index_pet_breeds_on_pet_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
@@ -39,6 +101,14 @@ ActiveRecord::Schema.define(version: 2022_05_24_201504) do
     t.index ["adopter_id"], name: "index_pets_on_adopter_id"
   end
 
+  create_table "pictures", force: :cascade do |t|
+    t.bigint "pet_id", null: false
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_id"], name: "index_pictures_on_pet_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -58,5 +128,26 @@ ActiveRecord::Schema.define(version: 2022_05_24_201504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "watchlists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_id"], name: "index_watchlists_on_pet_id"
+    t.index ["user_id"], name: "index_watchlists_on_user_id"
+  end
+
+  add_foreign_key "breed_preferences", "breeds"
+  add_foreign_key "breed_preferences", "users"
+  add_foreign_key "breed_webs", "breeds", column: "primary_breed_id"
+  add_foreign_key "breed_webs", "breeds", column: "similar_breed_id"
+  add_foreign_key "interests", "users"
+  add_foreign_key "matches", "pets"
+  add_foreign_key "matches", "users"
+  add_foreign_key "pet_breeds", "breeds"
+  add_foreign_key "pet_breeds", "pets"
   add_foreign_key "pets", "users", column: "adopter_id"
+  add_foreign_key "pictures", "pets"
+  add_foreign_key "watchlists", "pets"
+  add_foreign_key "watchlists", "users"
 end
