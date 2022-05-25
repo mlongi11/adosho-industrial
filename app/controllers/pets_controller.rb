e972f017eab1a1d0d76431eaa440262a538079d3
mlongi11 <mlongi@chicagobooth.edu>
2022-05-25 19:03:25 +0000

    completed a lot of webscraping, actively creates all available pets with competability ratings

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,22 +15,45 @@ class PetsController < ApplicationController
       @links.push(pet_link.attributes.fetch("href").to_s)
     end
 
-      pet_url = "https://www.pawschicago.org#{@links[1]}"
+    @links.each do |link|
+      pet_url = "https://www.pawschicago.org#{link}"
       pet_webpage = HTTP.get(pet_url)
       pet_parsed_page = Nokogiri::HTML(pet_webpage.body.to_s)
 
       pet = Pet.new
       pet.name = pet_parsed_page.css('.aqua-text').children.to_s
-      pet.adoption_center_identifier = @links[1]
-      pet.activity_competability_ranking = 1
-      pet.cat_competability_ranking =1
-      pet.children_competability_ranking = 1
-      pet.dog_competability_ranking = 1
-      pet.enrichment_competability_ranking = 1
-      pet.home_alone_competability_ranking = 1
-      pet.human_socialability_competability_ranking =1
-      pet.species = "Dog"
+      pet.adoption_center_identifier = link
+      pet.children_competability_ranking = if pet_parsed_page.css('.children .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.children .rating_default').children.to_s == ""
+        then nil 
+        else pet_parsed_page.css('.children .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.activity_competability_ranking = if pet_parsed_page.css('.activity .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.activity .rating_default').children.to_s == "" 
+        then nil 
+        else pet_parsed_page.css('.activity .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.cat_competability_ranking = if pet_parsed_page.css('.cats .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.cats .rating_default').children.to_s == ""
+        then nil 
+        else pet_parsed_page.css('.cats .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.dog_competability_ranking = if pet_parsed_page.css('.dogs .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.dogs .rating_default').children.to_s == "" 
+        then nil 
+        else pet_parsed_page.css('.dogs .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.enrichment_competability_ranking = if pet_parsed_page.css('.enrichment .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.enrichment .rating_default').children.to_s == ""
+        then nil 
+        else pet_parsed_page.css('.enrichment .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.home_alone_competability_ranking = if pet_parsed_page.css('.home_alone .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.home_alone .rating_default').children.to_s == ""
+        then nil 
+        else pet_parsed_page.css('.home_alone .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.human_socialability_competability_ranking = if pet_parsed_page.css('.human .rating_default').children.to_s == "UNKNOWN" || pet_parsed_page.css('.human .rating_default').children.to_s == "" 
+        then nil 
+        else pet_parsed_page.css('.human .rating_default').children.to_s.split("")[14].to_i 
+        end
+      pet.species = if link.include? "dog" then "Dog" else "Cat" end
       pet.save 
+    end
 
       # still need to pull
       #  estimated_birthday                        :date
